e0fe72503a699f34391ddf1348484e7f76c31fab
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-03-02 16:17:37 +0100

    added template file for hardcoded password

---
+++ PFXPasswordConst.inc.pas
@@ -0,0 +1,9 @@
+// Copy this file in a _PRIVATE folder (same level than 'src' folder) if you
+// want an EXE file with your password in the code and not typing it each time
+// you need to sign something.
+//
+// select RELEASE / PRIVATERELEASE before compile and deploy
+
+Const
+  CPFXCertificate = 'MyCertificat.pfx'; // name of your certificate file
+  CPFXPassword = 'MyPassword'; // password of the pfx file
