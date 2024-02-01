492a5bfa777a5a2093a655b275542ecfef900619
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-02-01 13:32:07 +0100

    upgrade to Delphi 12 Athens + released version 1.2 - 20240201

--- src/ExeBulkSigning.dpr
+++ src/ExeBulkSigning.dpr
@@ -7,12 +7,14 @@ uses
   Olf.RTL.Params in '..\lib-externes\librairies\Olf.RTL.Params.pas',
   u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
-  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm};
+  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
+  uDMProjectLogo in 'uDMProjectLogo.pas' {dmProjectLogo: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TdmProjectLogo, dmProjectLogo);
   Application.Run;
 end.
