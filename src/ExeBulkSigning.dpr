a6f4ea7d19ef56a408ba53235ac059e229f90fe8
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-02 15:45:04 +0200

    updated units list relative paths and added some

--- src/ExeBulkSigning.dpr
+++ src/ExeBulkSigning.dpr
@@ -4,11 +4,13 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   fMain in 'fMain.pas' {frmMain},
-  Olf.RTL.Params in '..\lib-externes\librairies\Olf.RTL.Params.pas',
-  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
-  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
-  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
-  uDMProjectLogo in 'uDMProjectLogo.pas' {dmProjectLogo: TDataModule};
+  uDMProjectLogo in 'uDMProjectLogo.pas' {dmProjectLogo: TDataModule},
+  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialog.pas',
+  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
+  Olf.RTL.CryptDecrypt in '..\lib-externes\librairies\src\Olf.RTL.CryptDecrypt.pas',
+  Olf.RTL.Params in '..\lib-externes\librairies\src\Olf.RTL.Params.pas',
+  u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
+  Olf.FMX.SelectDirectory in '..\lib-externes\Delphi-FMXExtend-Library\src\Olf.FMX.SelectDirectory.pas';
 
 {$R *.res}
 
