3e365adc727859dace4241558d71b36b4e482998
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-04 20:42:02 +0200

    finished C/S mode client library

--- src/ExeBulkSigningClientLib.pas
+++ src/ExeBulkSigningClientLib.pas
@@ -4,12 +4,15 @@ interface
 
 uses
   ExeBulkSigningClientServerAPI,
+  System.Generics.Collections,
   Olf.Net.Socket.Messaging;
 
 type
   TESBClient = class
   private
     FAPIClient: TExeBulkSigningClientServerAPIClient;
+    FWaitingFiles: TObjectQueue<TFileToSignMessage>;
+    FSentFiles: TDictionary<string, string>;
   protected
     FSessionID: string;
     procedure ReceiveLoginAnswerMessage(Const ASender: TOlfSMSrvConnectedClient;
@@ -20,17 +23,37 @@ type
       Const AMessage: TErrorMessage);
     procedure SendErrorMessage(Const ErrorText: string);
   public
+    /// <summary>
+    /// Check if the client is connected to a server.
+    /// </summary>
+    function isConnected: boolean;
+    /// <summary>
+    /// Send a file to the signing server.
+    /// </summary>
+    procedure SendFileToServer(const Title, URL, Filename: string);
+    /// <summary>
+    /// Returns the number of file sent to the server waiting for its response.
+    /// </summary>
+    function HasWaitingFiles: int64;
+    /// <summary>
+    /// Create an instance of the client and try to connect to a signing server.
+    /// </summary>
     constructor Create(Const AIP: string; Const APort: word;
       Const AAuthKey: string);
+    /// <summary>
+    /// Close the connection and free the memory used by this instance.
+    /// </summary>
     destructor Destroy; override;
   end;
 
 implementation
 
 uses
+  System.IOUtils,
   Olf.RTL.FileBuffer,
   System.SysUtils,
-  ExeBulkSigningClientServerAPIConsts;
+  ExeBulkSigningClientServerAPIConsts,
+  Olf.RTL.GenRandomID;
 
 { TESBClient }
 
@@ -41,6 +64,9 @@ var
 begin
   inherited Create;
   FSessionID := '';
+
+  FWaitingFiles := TObjectQueue<TFileToSignMessage>.Create(true);
+  FSentFiles := TDictionary<string, string>.Create;
 
   FAPIClient := TExeBulkSigningClientServerAPIClient.Create(AIP, APort);
   FAPIClient.onReceiveLoginAnswerMessage := ReceiveLoginAnswerMessage;
@@ -61,12 +87,88 @@ end;
 destructor TESBClient.Destroy;
 begin
   FAPIClient.Free;
+  FWaitingFiles.Free;
+  FSentFiles.Free;
   inherited;
+end;
+
+function TESBClient.HasWaitingFiles: int64;
+var
+  msg: TFileToSignMessage;
+{$IFDEF DEBUG}
+  Log: string;
+{$ENDIF}
+begin
+  result := 0;
+
+{$IFDEF DEBUG}
+  Log := tpath.Combine(tpath.GetDocumentsPath, 'ExeBulkSigningClientLog.txt');
+{$ENDIF}
+  tmonitor.enter(FWaitingFiles);
+  try
+    result := result + FWaitingFiles.Count;
+    if (result > 0) and (not FSessionID.IsEmpty) then
+    begin
+      while (FWaitingFiles.Count > 0) do
+      begin
+        msg := FWaitingFiles.Extract;
+        try
+{$IFDEF DEBUG}
+          tfile.WriteAllText(Log, tfile.ReadAllText(Log, tencoding.UTF8) +
+            slinebreak + 'Wait list to sent list : ' +
+            msg.FileNameWithItsExtension, tencoding.UTF8);
+{$ENDIF}
+          msg.SessionID := FSessionID;
+          FAPIClient.SendMessage(msg);
+          tmonitor.enter(FSentFiles);
+          try
+            FSentFiles.Add(msg.FileID, msg.FileNameWithItsExtension);
+          finally
+            tmonitor.Exit(FSentFiles);
+          end;
+        finally
+          msg.Free;
+        end;
+      end;
+      result := 0;
+    end;
+  finally
+    tmonitor.Exit(FWaitingFiles);
+  end;
+
+  tmonitor.enter(FSentFiles);
+  try
+    result := result + FSentFiles.Count;
+  finally
+    tmonitor.Exit(FSentFiles);
+  end;
+
+{$IFDEF DEBUG}
+  tfile.WriteAllText(Log, tfile.ReadAllText(Log, tencoding.UTF8) + slinebreak +
+    'Nb files to sign : ' + result.tostring, tencoding.UTF8);
+{$ENDIF}
+end;
+
+function TESBClient.isConnected: boolean;
+begin
+  result := FAPIClient.isConnected;
 end;
 
 procedure TESBClient.ReceiveErrorMessage(const ASender
   : TOlfSMSrvConnectedClient; const AMessage: TErrorMessage);
+{$IFDEF DEBUG}
+var
+  Log: string;
+{$ENDIF}
 begin
+{$IFDEF DEBUG}
+  Log := tpath.Combine(tpath.GetDocumentsPath, 'ExeBulkSigningClientLog.txt');
+{$ENDIF}
+{$IFDEF DEBUG}
+  tfile.WriteAllText(Log, tfile.ReadAllText(Log, tencoding.UTF8) + slinebreak +
+    '*** ERROR *** ' + AMessage.ErrorCode.tostring + ' - ' + AMessage.ErrorText,
+    tencoding.UTF8);
+{$ENDIF}
   // TODO : prévenir l'UI qu'une erreur a été détectée
 end;
 
@@ -77,14 +179,48 @@ begin
     raise exception.Create('Can''t login.');
 
   FSessionID := AMessage.SessionID;
-
-  // TODO : prévenir l'UI que la connexion est OK
 end;
 
 procedure TESBClient.ReceiveSignedFileMessage(const ASender
   : TOlfSMSrvConnectedClient; const AMessage: TSignedFileMessage);
+var
+  Filename: string;
+{$IFDEF DEBUG}
+var
+  Log: string;
+{$ENDIF}
 begin
-  // TODO : à compléter
+{$IFDEF DEBUG}
+  Log := tpath.Combine(tpath.GetDocumentsPath, 'ExeBulkSigningClientLog.txt');
+{$ENDIF}
+  tmonitor.enter(FSentFiles);
+  try
+    if FSentFiles.TryGetValue(AMessage.FileID, Filename) then
+      FSentFiles.Remove(AMessage.FileID)
+    else
+      Filename := '';
+  finally
+    tmonitor.Exit(FSentFiles);
+  end;
+{$IFDEF DEBUG}
+  tfile.WriteAllText(Log, tfile.ReadAllText(Log, tencoding.UTF8) + slinebreak +
+    'Signed file received : ' + AMessage.FileID + ' - ' + Filename,
+    tencoding.UTF8);
+{$ENDIF}
+  if Filename.IsEmpty then
+  begin
+    // TODO : signaler anomalie au niveau de l'UI
+{$IFDEF DEBUG}
+    tfile.WriteAllText(Log, tfile.ReadAllText(Log, tencoding.UTF8) + slinebreak
+      + '*** ERROR *** ' + AMessage.FileID + ' not found in SentFiles list.',
+      tencoding.UTF8);
+{$ENDIF}
+  end
+  else
+    AMessage.FileBuffer.SaveToFile
+      (tpath.Combine(tpath.GetDirectoryName(Filename),
+      tpath.GetFileNameWithoutExtension(Filename) + '-signed' +
+      tpath.GetExtension(Filename)));
 end;
 
 procedure TESBClient.SendErrorMessage(const ErrorText: string);
@@ -93,6 +229,7 @@ var
 begin
   msg := TErrorMessage.Create;
   try
+    msg.ErrorCode := 0;
     msg.ErrorText := ErrorText;
     FAPIClient.SendMessage(msg);
   finally
@@ -100,6 +237,73 @@ begin
   end;
 
   raise exception.Create(ErrorText);
+end;
+
+procedure TESBClient.SendFileToServer(const Title, URL, Filename: string);
+var
+  FileID: string;
+  msg: TFileToSignMessage;
+{$IFDEF DEBUG}
+  Log: string;
+{$ENDIF}
+begin
+{$IFDEF DEBUG}
+  Log := tpath.Combine(tpath.GetDocumentsPath, 'ExeBulkSigningClientLog.txt');
+{$ENDIF}
+  if not tfile.Exists(Filename) then
+    raise exception.Create('File "' + Filename + '" doesn''t exist !');
+
+  if FSessionID.IsEmpty then
+  begin
+    msg := TFileToSignMessage.Create;
+    try
+      msg.SignToolTitle := Title;
+      msg.SignToolURL := URL;
+      msg.SessionID := '';
+      msg.FileNameWithItsExtension := Filename;
+      msg.FileID := TOlfRandomIDGenerator.getIDBase62(50);
+      msg.FileBuffer.LoadFromFile(Filename);
+      tmonitor.enter(FWaitingFiles);
+      try
+        FWaitingFiles.Enqueue(msg);
+{$IFDEF DEBUG}
+        tfile.WriteAllText(Log, tfile.ReadAllText(Log, tencoding.UTF8) +
+          slinebreak + 'Wait list : ' + msg.FileNameWithItsExtension,
+          tencoding.UTF8);
+{$ENDIF}
+      finally
+        tmonitor.Exit(FWaitingFiles);
+      end;
+    except
+      msg.Free;
+    end;
+  end
+  else
+  begin
+    msg := TFileToSignMessage.Create;
+    try
+      msg.SignToolTitle := Title;
+      msg.SignToolURL := URL;
+      msg.SessionID := FSessionID;
+      msg.FileNameWithItsExtension := Filename;
+      msg.FileID := TOlfRandomIDGenerator.getIDBase62(50);
+      msg.FileBuffer.LoadFromFile(Filename);
+      FAPIClient.SendMessage(msg);
+      tmonitor.enter(FSentFiles);
+      try
+        FSentFiles.Add(msg.FileID, msg.FileNameWithItsExtension);
+{$IFDEF DEBUG}
+        tfile.WriteAllText(Log, tfile.ReadAllText(Log, tencoding.UTF8) +
+          slinebreak + 'Sent list : ' + msg.FileNameWithItsExtension,
+          tencoding.UTF8);
+{$ENDIF}
+      finally
+        tmonitor.Exit(FSentFiles);
+      end;
+    finally
+      msg.Free;
+    end;
+  end;
 end;
 
 end.
